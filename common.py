2fc5f4ceeb20d5b1231de39a1c4f49ffc9a1f77a
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-07-10 12:39:34 +1000

    feat: implement message loading for each individual channel

--- common.py
+++ common.py
@@ -1,7 +1,8 @@
 from PyQt6.QtCore import QObject, pyqtSignal
-from PyQt6.QtWidgets import QVBoxLayout, QScrollArea
+from PyQt6.QtWidgets import QVBoxLayout, QWidget
 
 
 class MessagesManager(QObject):
-    messages_updated = pyqtSignal(list)  # Signal carrying a list of messages
-    messages_frame: tuple[QScrollArea, QVBoxLayout] | None = None
+    # The signal contains the channel ID and a list of messages
+    messages_updated = pyqtSignal(str, list)  # Signal carrying a list of messages
+    messages_frame: tuple[QWidget, dict] | None = None
