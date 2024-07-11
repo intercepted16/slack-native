e2e291695f14244987aa83edcfb0ba9c1894278b
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-07-11 10:51:57 +1000

    refactor: switch to PySide6
    This commit switches from PyQt6 to PySide6 for its official support and the more permissive LGPL license. This allows the program to use the MIT license without any constraints.

--- common.py
+++ common.py
@@ -1,8 +1,8 @@
-from PyQt6.QtCore import QObject, pyqtSignal
-from PyQt6.QtWidgets import QWidget
+from PySide6.QtCore import QObject, Signal
+from PySide6.QtWidgets import QWidget
 
 
 class MessagesManager(QObject):
     # The signal contains the channel ID and a list of messages
-    messages_updated = pyqtSignal(str, list)  # Signal carrying a list of messages
+    messages_updated = Signal(str, list)  # Signal carrying a list of messages
     messages_frame: tuple[QWidget, dict] | None = None
