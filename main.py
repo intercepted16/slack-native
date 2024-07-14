3d498b7859e64692bc9320fc4e5cc44773473220
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-07-14 15:45:22 +1000

    feat: system tray functionality
    This commit implements system tray functionality. It uses the inbuilt system tray APIs of PySide6. It uses a `QSharedMemory` workaround to detect multiple instances of the application and spawns an error message if attempted. It also fixes the application not fully exiting (including the Flask thread) on quit.

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 from app import main
 from oauth import main as flask_app
 import threading
-import sys
 
 if __name__ == '__main__':
     app, window, messages_manager = main()
     flask_thread = threading.Thread(target=flask_app, args=[messages_manager])
     flask_thread.start()
-    window.show()
-    sys.exit(app.exec())
