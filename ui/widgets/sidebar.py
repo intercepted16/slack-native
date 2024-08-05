6c2ead456cc6c813f93d7212d48c1b512896ecec
intercepted16 <162676516+intercepted16@users.noreply.github.com>
2024-08-05 19:56:54 +1000

    fix: incorrect indexes being shown in lazy loaded content stack

--- ui/widgets/sidebar.py
+++ ui/widgets/sidebar.py
@@ -5,25 +5,22 @@ from typing import List, Union
 from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton, QStackedWidget
 from qt_async_threads import QtAsyncRunner
 
+lazy_loaded = {}
+
 
 # make a decorator to run when a button is clicked
-async def on_button_click(contentStack: QStackedWidget, i, widget_resolver: callable, func: callable):
+async def on_button_click(contentStack: QStackedWidget, i, func: callable):
     # Print debug information about current widget and index
     print(f"Index: {i}")
-    widget_at_index = contentStack.widget(i)
-    print(f"Widget at index {i}: {widget_at_index}")
+    widget = contentStack.widget(i)
+    print(f"Widget at index {i}: {widget}")
 
-    # Check if the widget exists at the specified index
-    if widget_at_index is not None:
-        print("Widget already exists")
+    if lazy_loaded.get(i):
+        print("Widget already loaded")
         contentStack.setCurrentIndex(i)
         return
 
-    # Resolve and add the widget
-    if inspect.iscoroutinefunction(widget_resolver):
-        widget = await widget_resolver()
-    else:
-        widget = widget_resolver()
+    lazy_loaded[i] = True
 
     # Run the function if provided
     if inspect.isfunction(func):
@@ -37,8 +34,8 @@ async def on_button_click(contentStack: QStackedWidget, i, widget_resolver: call
     # Print debug information before inserting the widget
     print(f"Inserting widget at index {i}")
     print(f"Widget to insert: {widget}")
-    contentStack.setCurrentIndex(i)
-    contentStack.addWidget(widget)
+    index = contentStack.insertWidget(i, widget)
+    print(f"Inserted widget at index {index}")
     contentStack.setCurrentIndex(i)
 
     # Print all widgets in contentStack for verification
@@ -58,9 +55,11 @@ class SideBar(QWidget):
         self.runner = QtAsyncRunner()
 
         for i, (button, widget_resolver, func) in enumerate(self.buttons):
+            # add the initial widget
+            self.contentStack.insertWidget(i, widget_resolver())
             self.layout.addWidget(button)
             button.clicked.connect(
-                partial(self.runner.to_sync(on_button_click), self.contentStack, i, widget_resolver, func))
+                partial(self.runner.to_sync(on_button_click), self.contentStack, i, func))
 
         self.layout.addStretch(1)
         self.setLayout(self.layout)
